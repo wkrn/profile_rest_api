6a570fb4609eb6130a6acd842261227ec7d72a97
RW <maggots00002@gmail.com>
2019-04-06 22:06:13 +0900

    Create first api view

---
+++ src/profiles_project/profiles_api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns =[
+    path('hello-view/', views.HelloApiView.as_view()),
+
+]
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloApiView(APIView):
+    """TEST API VIEW"""
+
+    def get(self, request, format=None):
+        """Return a list of APIView feature"""
+
+        an_apiview = [
+        """これはこれこれこうですが、コメントアウトしているはずです"""
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
