08b9c8e1273480e3cc8dca16c96b1c96ea908476
RW <maggots00002@gmail.com>
2019-04-06 23:49:14 +0900

    Added profile listing and creating feature to pur rest api

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet, base_name='profile')
 
 urlpatterns =[
     path('hello-view/', views.HelloApiView.as_view()),
